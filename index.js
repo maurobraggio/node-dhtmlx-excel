bc5e72119642385037bd0bc6b2456ec5ef3574fe
Mauro Braggio <m.braggio@e-time.it>
2017-07-26 16:45:38 +0200

    fix export with only a row

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var inspect = require('util').inspect;
 exports.start = function(xmlData) {
     var json_data = convert.xml2json(xmlData, {compact: true, spaces: 4});
     var obj = JSON.parse(json_data);
-    
+
     // Load a new blank workbook
     return XlsxPopulate.fromBlankAsync()
         .then(workbook => {
@@ -13,17 +13,42 @@ exports.start = function(xmlData) {
             for ( idColumn in obj.rows.head.columns.column ) {
                 //
                 var id = parseInt(idColumn)+1;
-                workbook.sheet(0).cell(1,id).value( obj.rows.head.columns.column[idColumn]._cdata ) ;
+                var cell = obj.rows.head.columns.column[idColumn];
+
+                if (cell._attributes) {
+                    if ( cell._attributes.color ) {
+                        var color = cell._attributes.color.substr(1);
+                        workbook.sheet(0).column(id).style( "fill", color );
+                    }
+                    if ( cell._attributes.width ) {
+                        workbook.sheet(0).column(id).width( parseInt(cell._attributes.width)/4 );
+                    }
+                }
+                workbook.sheet(0).cell(1,id).value( cell._cdata  ) ;
+
             }
-        
+
+            //Bold & underline First row
+            workbook.sheet(0).row(1).style("bold", true);
+            workbook.sheet(0).row(1).style("bottomBorder", {style: "medium"});
+
+            var rows = [];
+            // fix if only one row
+            if (!Array.isArray(obj.rows.row)){
+                rows.push(obj.rows.row);
+            }
+            else {
+                rows = obj.rows.row;
+            }
+
             var rowCount = 2;
             // Rows
-            for ( idRow in obj.rows.row ) {
-                var currentRow = obj.rows.row[idRow];
+            for ( idRow in rows ) {
+                var currentRow = rows[idRow];
                 for ( idColumn in currentRow.cell ) {
-                     var currentColumn = currentRow.cell[idColumn];
-                     var id = parseInt(idColumn)+1;
-                     workbook.sheet(0).cell(rowCount,id).value( currentColumn._cdata ) ;
+                    var currentColumn = currentRow.cell[idColumn];
+                    var id = parseInt(idColumn)+1;
+                    workbook.sheet(0).cell(rowCount,id).value( currentColumn._cdata ) ;
                 }
                 rowCount++;
             }
@@ -33,5 +58,5 @@ exports.start = function(xmlData) {
 
 
 exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+    console.log("This is a message from the demo package");
 }
